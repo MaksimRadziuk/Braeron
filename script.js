702865d0aa580011571edc6fbce8aaa728305200
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2019-12-07 02:34:49 +0300

    07/12 2.34

--- script.js
+++ script.js
@@ -23,6 +23,48 @@ $(document).ready(function(){
 			}
 		]
 	});
+	$('.history_slider').slick({
+		slidesToShow:3,
+		slidesToScroll:1,
+		autoplay:false,
+		autoplaySpeed:5000,
+		speed:1500,
+		arrows:true,
+		infinite:true,
+		dots:false,
+		responsive:[
+			{
+			breakpoint:1231,
+			settings:{
+				slidesToShow:2
+				}
+			}
+		]
+	});
+	$('#certSlider').slick({
+		slidesToShow:4,
+		slidesToScroll:1,
+		autoplay:false,
+		autoplaySpeed:5000,
+		speed:1500,
+		arrows:true,
+		infinite:true,
+		dots:false,
+		responsive:[
+			{
+			breakpoint:1231,
+			settings:{
+				slidesToShow:3
+				}
+			},
+			{
+			breakpoint:901,
+			settings:{
+				slidesToShow:2
+				}
+			}
+		]
+	});
 
 	$('.closePopup').click(function() {
 		$(this).parent('.popup').toggleClass('active');
